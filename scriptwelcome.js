b8242887fbc3e6aba21426f4b7bd84a9e406e8f8
Errorist <146121333+iloveyourmayo@users.noreply.github.com>
2024-10-08 18:35:16 +0800

    Update scriptwelcome.js

--- scriptwelcome.js
+++ scriptwelcome.js
@@ -1,10 +1,13 @@
 function showWelcomeAlert() {
   const message = "Hi O.S Guest! Click OK to begin FCFS Process Scheduling.";
-  const result = confirm(message);
 
-  if (result) {
-    window.location.href = "index2.html";
-  }
+  setTimeout(() => {
+    const result = confirm(message);
+
+    if (result) {
+      window.location.href = "index2.html"; 
+    }
+  }, 3000); 
 }
 
 window.addEventListener("load", showWelcomeAlert);
